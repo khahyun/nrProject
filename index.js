d3b4237461bd9b2bf139931b3ac9a96de3158110
khahyun <hahyeon1@hanmail.net>
2021-01-02 01:22:56 +0900

    login route

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = 5000
 
 //user모델가져오기
 const bodyParser = require('body-parser') 
+const cookieParser = require('cookie-parser') 
 const config = require('./config/key');
 
 const {User} = require("./models/User")
@@ -15,8 +16,10 @@ const {User} = require("./models/User")
 app.use(bodyParser.urlencoded({ extended: true }));
 //application/json 
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 const mongoose = require('mongoose')
+
 mongoose.connect(config.mongoURI,{
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected..'))
@@ -41,6 +44,34 @@ app.post('/register',(req,res) => {
         })
     })
 }) 
+
+app.post('/login',(req, res) => {
+  //요청된 이메일을 디비에서 찾기
+  User.findOne({ email: req.body.email }, (err, user) =>{
+    if(!user){
+      return res.json({
+        loginSuccess : false,
+        message: "가입된 이메일이 없습니다."
+      })
+    }
+
+                         //plain password
+    user.comparePassword( req.body.password , (err, isMatch) => {
+      if(!isMatch)
+        return res.json({ loginSuccess: false, message: "비밀번호가 틀렸습니다."})
+
+      user.generateToken((err, user) => {
+          if(err) return res.status(400).send(err)
+          
+            // token 을 쿠키에 저장한다
+          res.cookie("x_auth", user.token)
+            .status(200)
+            .json({loginSuccess : true, userId : user._id})
+            //json 으로 데이터 보내기!
+      })
+    })
+  })
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
--- models/User.js
+++ models/User.js
@@ -3,6 +3,7 @@ const bcrypt = require('bcrypt');
 //salt 를 이용해서 비밀번호를 암호화. saltrounds글자수 salt를 먼저생성. 
 const saltRounds = 10
 
+const jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
         name: {
@@ -58,6 +59,31 @@ const userSchema = mongoose.Schema({
     })
 
 
+//비밀번호 비교
+userSchema.methods.comparePassword = function(plainPassword, cb){
+
+    //plain을 다시 암호화해서 디비 비번이랑 비교해야한다.
+
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch){
+        if(err) return cb(err); 
+        cb(null, isMatch);
+    })
+}
+
+//토큰생성
+userSchema.methods.generateToken = function(cb){
+    var user = this;
+    // json web token 이용하여 token 생성하기
+    // user id 와 두번째 param 으로 토큰을 만들고, param 을 이용하여 나중에 userid를 찾아낸다. 
+                            //plain object로 만들기
+  var token = jwt.sign(user._id.toHexString(), "secretToken")
+
+  user.token = token
+  user.save( function (err, user) {
+   if(err) return cb(err);
+   cb(null,user);
+})
+}
 //다른 곳에서도 쓸 수 있도록
 const User = mongoose.model('User', userSchema)
 
